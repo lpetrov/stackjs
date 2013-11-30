ba07003e03b3619b2ef0555fde9f42013b682d6d
Lyubomir Petrov <lpetrov@me.com>
2013-11-30 16:51:44 +0200

    comments;

--- examples/complex-ui/tasklist.js
+++ examples/complex-ui/tasklist.js
@@ -1,3 +1,25 @@
+/**
+ * Example TODO MVC app, build using Stack's helpers for creating jQuery plugins.
+ *
+ * Important notes:
+ *  - if `allow_multiple_instances` is enabled (as in this case) only .refresh_ui() will be called when you try to
+ *  initialise the plugin again on the same DOM element
+ *  - as you can see, for mutable data, `Stack.BaseComponent.data()` is used.
+ *  - for immutable data we use: `Stack.BaseComponent.defaults`, `Stack.Config` and `Stack.BaseComponent.option()`
+ *  - the `.proxyEvent` helper is used to trigger a `$prefix:$eventName` on the current `TaskListPlugin` instance, when
+ *  some of the proxied events are triggered on the `TaskPlugin` instances
+ *  - NO global vars are used, everything is scoped into self.*
+ *  - Custom workflow is implemented using simple methods, called from the init() method:
+ *      1. `build` - build and prepare any DOM elements which are or may be needed by the component (jQ plugin)
+ *         - called once
+ *      2. `events` - bind/unbind any events...everything is IN the scope of the $element
+ *          - called once
+ *      3. `refresh_ui` - sync any `.data()` stuff w/ the presentation of the DOM elements (part of Stack's BasePlugin)
+ *          - may be called multiple times (see `allow_multiple_instances`)
+ *
+ *      (This can/should be abstracted into your own BasePlugin version)
+ *
+ */
 (function(Stack, $, undefined) {
     var TaskListPlugin = Stack.jQueryPlugins.BasePlugin.extend({
         'name': 'tasklist',
@@ -16,16 +38,6 @@
             var self = this;
             var $element = $(self.element); // this is the element that we'd attached the plugin to.
 
-
-            /**
-             * Simple workflow...:
-             * 1. build - build and prepare any DOM elements which are or may be needed by the component (jQ plugin)
-             *  - called once
-             * 2. events - bind/unbind any events...everything is IN the scope of the $element
-             *  - called once
-             * 3. refresh_ui - sync any .data() stuff w/ the presentation of the DOM elements
-             *  - may be called multiple times
-             */
             self.build();
             self.events();
             self.refresh_ui();
